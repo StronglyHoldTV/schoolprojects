5e7b78751659e70c946aa55fde4011580d8921e5
Marek Villim <strongly.hold.rtvs@gmail.com>
2023-10-04 09:08:45 +0200

    asf

---
+++ INF-4/_3.py
@@ -0,0 +1,17 @@
+##a  =  """Lorem ipsum dolor sit amet,
+## consectetur adipiscing elit.
+## Phasellus lobortis dui eu quam maximus,
+## volutpat scelerisque ligula vulputate.
+## Maecenas volutpat metus massa, et malesuada
+## erat rutrum posuere. Curabitur vel eros nec
+## dui iaculis bibendum eget vel massa."""
+##
+##def dlzka(re):
+##    """Docu docu docu docu???"""
+##    return len(re)
+##
+##r = input('Zadaj retazec: ')
+##print('t' in r)
+##print(dlzka(r))
+##
+##"asfasf".replace('a', 'b')
