7c8f7a883c31864cd80530d9ee6897c9c4f89dd8
Marek Villim <strongly.hold.rtvs@gmail.com>
2023-10-18 08:44:38 +0200

    1

--- INF-4/2/1n.py
+++ INF-4/2/1n.py
@@ -1,16 +1,26 @@
-cislo = input("Zadajte cislo v desiatkovej sustave")
-zakladSustavy = int(input("Do akej sústavi prevádzaš?"))
-znaky = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-vysledok = 0;
+def prevod(cislo, zaklad):
+    vysledok = ""
+    znaky = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (zaklad <= 1 or zaklad > len(znaky)):
+        raise Exception(f'Zaklad is less than 1 and more than {len(znaky)}')
+    while(cislo > 0):
+        modulo = cislo%zaklad
+        vysledok += znaky[modulo]
+        cislo = cislo//zaklad
+    return vysledok[::-1]
 
-if (zakladSustavy > 1 and zakladSustavy <= len(znaky)):
-    pozice = 0;
-    for i in range(len(cislo)-1, 0, -1):
-        cifra = znaky.index(cislo[i])
-        print(i)
-        print(cislo[i])
-        print(znaky.index(cislo[i]))
-        print(cifra)
-        vysledok += cifra * (int)(zakladSustavy ** pozice);
-        pozice += 1
-print("Vysledok", vysledok)
+def prevodtodec(cislo, zaklad):
+    znaky = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    vysledok = 0
+    for no, i in enumerate(cislo[::-1]):
+        vysledok += znaky.index(i) * (zaklad** (no))
+    return vysledok
+    
+
+while True:
+   # a = int(input("Zadajte cislo v desiatkovej sustave"))
+   # b = int(input("Do akej sústavi prevádzaš?"))
+    #print(prevod(a, b))
+   a = input("Zadajte cislo v nejakej sustave")
+   b = int(input("V akej sustave je to cislo?"))
+   print(prevodtodec(a, b))
